273226615d1fcbe22cbafc99d1f1462d4e0bd447
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-04-08 21:57:04 +0800

    short circuiting

--- script.js
+++ script.js
@@ -179,3 +179,20 @@ add(5, 6, 7, 8, 9, 10, 11);
 
 const arrNew = [23, 5, 7];
 add(...arrNew);
+
+//use any data type, return any data type, short circuiting
+
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('------ AND ------');
